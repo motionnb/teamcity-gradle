0c5038bfefa476ee0b42e0f2cb6eb58e62bae6c4
Marco Behler <marco.behler@jetbrains.com>
2021-03-01 12:55:54 +0100

    initial commit

---
+++ src/test/java/com/example/teamcitygradle/TeamcityGradleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teamcitygradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamcityGradleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
